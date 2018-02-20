a4962ce0bc397889fdeb2951f568cdf9c9736fce
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-02-21 04:51:54 +0530

    Custom DialogBox added

--- app/src/main/java/com/example/shashankmohabia/morphme/MainActivity.kt
+++ app/src/main/java/com/example/shashankmohabia/morphme/MainActivity.kt
@@ -1,25 +1,49 @@
 package com.example.shashankmohabia.morphme
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_popup.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
+            R.id.profile -> {
                 message.setText("Profile")
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.dashboard -> {
+                val intent = Intent(this, GameActivity::class.java)
+                startActivity(intent)
+
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText("Logout")
+            R.id.logout -> {
+                val mydialog = Dialog(this)
+                mydialog.setContentView(R.layout.custom_popup)
+                var yes = mydialog.findViewById<Button>(R.id.yesButton)
 
+                var no = mydialog.findViewById<Button>(R.id.noButton)
+                yes.setOnClickListener(View.OnClickListener {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                })
+                no.setOnClickListener(View.OnClickListener {
+                    mydialog.dismiss()
+                })
+                mydialog.show()
                 return@OnNavigationItemSelectedListener true
             }
         }
