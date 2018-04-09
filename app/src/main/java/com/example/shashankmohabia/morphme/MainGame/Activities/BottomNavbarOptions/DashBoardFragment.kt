51b81511046dfc235ce6f2baef50e3735a36acf0
Shashank Mohabia <shashankmohabia27@gmail.com>
2018-04-10 01:05:38 +0530

    Score card added in DashBoard

--- app/src/main/java/com/example/shashankmohabia/morphme/MainGame/Activities/BottomNavbarOptions/DashBoardFragment.kt
+++ app/src/main/java/com/example/shashankmohabia/morphme/MainGame/Activities/BottomNavbarOptions/DashBoardFragment.kt
@@ -2,6 +2,7 @@ package com.example.shashankmohabia.morphme.MainGame.Activities.BottomNavbarOpti
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,12 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashBoardFragment : Fragment() {
 
-    var phase1Score: Int = 0
-    var phase2Score: Int = 0
+    var phase1Score: Long = 0
+    var phase2Score: Long = 0
     var userID: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +34,31 @@ class DashBoardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         calculateScore()
+
+    }
+
+    private fun showScore() {
+        Phase1Score.text = phase1Score.toString()
+        Phase2Score.text = phase2Score.toString()
     }
 
     private fun calculateScore() {
-        val userDb = FirebaseDatabase.getInstance().reference.child("Users").child(userID).child("Responses").child("Correct")
+        val userDb = FirebaseDatabase.getInstance().reference.child("Users").child(userID)
         userDb.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError?) {
             }
 
             override fun onDataChange(dataSnapshot: DataSnapshot?) {
+                if (dataSnapshot != null) {
+                    if (dataSnapshot.exists()) {
+                        phase1Score = dataSnapshot.child("scorePhase1").value as Long
+                        phase2Score = dataSnapshot.child("scorePhase2").value as Long
+                       // Log.d("dash", phase1Score.toString())
+                        showScore()
+                    }
+                }
             }
         })
+
     }
 }
